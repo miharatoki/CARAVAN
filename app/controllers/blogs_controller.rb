ca52baa5d0c432f9bcb00c7a8e48455a1c20da0c
mihartoki <ec2-user@ip-172-31-22-143.ec2.internal>
2021-07-02 11:28:42 +0000

    (finish)3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,17 +1,19 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
   end
 
   def new
+    @blog = Blog.new
   end
   
   def create
-    @blog = Blog.new(blog_params)
+    blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_pash
+    redirect_to blogs_path
   end
 
   def edit
@@ -19,6 +21,6 @@ class BlogsController < ApplicationController
   
   private   
   def blog_params
-    params.require(:blog).permie(:title, :category, :body)
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
